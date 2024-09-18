26518a2b3493e93ba66431aca903ba1f0fa11849
DeanTheBean958 <dean.pauls@stu.ocsb.ca>
2024-09-18 13:59:03 +0000

    1

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="stylesheet" href="styles.css">
+  <title>Somthing</title>
+</head>
+
+        <body>
+
+          <h1>type words below</h1>
+          <!-- Simple Textfield -->
+          <form action="#">
+            <div class="mdl-textfield mdl-js-textfield">
+              <input class="mdl-textfield__input" type="text" id="sample1">
+              <label class="mdl-textfield__label" for="sample1">Text...</label>
+
+              <!-- Colored FAB button with ripple -->
+              <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+                <i class="material-icons">click</i>
+              </button>
+
+        <!-- Default Slider -->
+        <p style="width:300px">
+          <input class="mdl-slider mdl-js-slider" type="range" id="s1" min="0" max="10" value="4" step="2">
+        </p>
+
+      </div>
+    </section>
+    <section class="mdl-layout__tab-panel" id="fixed-tab-3">
+      <div class="page-content">
+
+        <!-- Simple list -->
+        <style>
+          .demo-list-item {
+            width: 300px;
+          }
+        </style>
+
+        <ul class="demo-list-item mdl-list">
+          <li class="mdl-list__item">
+            <span class="mdl-list__item-primary-content">
+              Bryson mozzarella sticks
+            </span>
+          </li>
+          <li class="mdl-list__item">
+            <span class="mdl-list__item-primary-content">
+              enriceo
+            </span>
+          </li>
+          <li class="mdl-list__item">
+            <span class="mdl-list__item-primary-content">
+              sam :(
+            </span>
+          </li>
+        </ul>
+
+      </div>
+    </section>
+  </main>
+</div>
+</body>
+</html>
