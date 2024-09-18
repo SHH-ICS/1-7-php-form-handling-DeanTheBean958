8e499a46ed078894f68774ce563c8d785b6a9fcc
DeanTheBean958 <dean.pauls@stu.ocsb.ca>
2024-09-18 14:07:58 +0000

    2

--- index.php
+++ index.php
@@ -10,58 +10,20 @@
   <title>Somthing</title>
 </head>
 
-        <body>
+<body>
 
-          <h1>type words below</h1>
-          <!-- Simple Textfield -->
-          <form action="#">
-            <div class="mdl-textfield mdl-js-textfield">
-              <input class="mdl-textfield__input" type="text" id="sample1">
-              <label class="mdl-textfield__label" for="sample1">Text...</label>
+  <form action="welcome.php" method="post">
+    Name: <input type="text" name="name"><br>
+    age: <input type="text" name="age"><br>
+    where do you live <input.Type="text" name="live"><br>
+    <input type="submit">
+  </form>
 
-              <!-- Colored FAB button with ripple -->
-              <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
-                <i class="material-icons">click</i>
-              </button>
+  Welcome <?php echo $_POST["name"]; ?><br>
+  your age is: <?php echo $_POST["age"]; ?>
+  you live at: <?php.echo $_POST["live"]; ?>
 
-        <!-- Default Slider -->
-        <p style="width:300px">
-          <input class="mdl-slider mdl-js-slider" type="range" id="s1" min="0" max="10" value="4" step="2">
-        </p>
 
-      </div>
-    </section>
-    <section class="mdl-layout__tab-panel" id="fixed-tab-3">
-      <div class="page-content">
-
-        <!-- Simple list -->
-        <style>
-          .demo-list-item {
-            width: 300px;
-          }
-        </style>
-
-        <ul class="demo-list-item mdl-list">
-          <li class="mdl-list__item">
-            <span class="mdl-list__item-primary-content">
-              Bryson mozzarella sticks
-            </span>
-          </li>
-          <li class="mdl-list__item">
-            <span class="mdl-list__item-primary-content">
-              enriceo
-            </span>
-          </li>
-          <li class="mdl-list__item">
-            <span class="mdl-list__item-primary-content">
-              sam :(
-            </span>
-          </li>
-        </ul>
-
-      </div>
-    </section>
-  </main>
-</div>
 </body>
+
 </html>
